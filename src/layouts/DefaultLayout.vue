3acae70de48899f6eb498de77f798afe36435e6f
miket-llc <mdt_@msn.com>
2022-07-17 19:45:32 -0400

    Added SplitPanes. Stubbed out Layouts.

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,11 @@
+<splitpanes style="height: 400px">
+  <pane min-size="20">1</pane>
+  <pane>
+    <splitpanes horizontal>
+      <pane>2</pane>
+      <pane>3</pane>
+      <pane>4</pane>
+    </splitpanes>
+  </pane>
+  <pane>5</pane>
+</splitpanes>
